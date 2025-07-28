bc1608b573b609db3e72d6a5471490fdb3ef0ded
ariane-s-f16 <arianedesouzaficho@gmail.com>
2025-07-28 15:22:58 -0300

    terminar

--- MauiAPP3BIMESTRE/Helper/SQLiteDataBaseHelper.cs
+++ MauiAPP3BIMESTRE/Helper/SQLiteDataBaseHelper.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using SQLite;
-using MauiAppFit.Models;
+using MauiAPP3BIMESTRE.Models;
 
 namespace MauiAPP3BIMESTRE.Helper
 {
@@ -15,6 +15,14 @@ namespace MauiAPP3BIMESTRE.Helper
         {
             _db = new SQLiteAsyncConnection(dbPath);
             _db.CreateTableAsync<Atividade>().Wait();
+        }
+        public Task<Atividade> GetbyId(int id) 
+        {
+            return _db.Table<Atividade>().FirstAsync(i=i=>i.Id == id);
+        }
+        public Task<int> Insert(int id)
+        {
+            return _db.InsertAsync(model);
         }
     }
 }
